b0bb342fefe9c9efd92746a8c121b02cf4f318ce
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2022-08-10 18:46:14 +0200

    wuibwef

--- src/script.js
+++ src/script.js
@@ -5,6 +5,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
+
+import Stats from 'stats.js'
+
+const stats = new Stats()
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
 
 /**
  * Base
@@ -71,11 +78,10 @@ const gltfLoader = new GLTFLoader()
 dracoLoader.setDecoderPath('/draco/')
 gltfLoader.setDRACOLoader(dracoLoader)
 
-const textureLoader = new THREE.TextureLoader()
-const texture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_baseColor.jpeg')
-const metallicTexture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_metallicRoughness.png')
-
-texture.minFilter = THREE.LinearFilter
+// const textureLoader = new THREE.TextureLoader()
+// const texture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_baseColor.jpeg')
+// const metallicTexture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_metallicRoughness.png')
+// texture.minFilter = THREE.LinearFilter
 
 gltfLoader.load('models/mont_blanc_massif_photographed_from_iss/scene.gltf', (gltf) => {
     const childrens = [...gltf.scene.children]
@@ -101,6 +107,51 @@ torus.position.set(-6.037, 4.846, -4.747)
 torus.scale.set(0.198, 0.198, 0.198)
 
 scene.add(torus);
+
+
+
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+const raycaster = new THREE.Raycaster()
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / sizes.width) * 2 - 1
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+window.addEventListener('click', (event) => {
+    if (currentIntersect) {
+        console.log(currentIntersect);
+        currentIntersect.object.material.color.set(0xff00ff)
+
+        const coordinates = {
+            x: currentIntersect.object.position.x + 5,
+            y: currentIntersect.object.position.y,
+            z: currentIntersect.object.position.z
+        }
+
+        gsap.to(camera.position, {
+            x: coordinates.x,
+            y: coordinates.y,
+            z: coordinates.z,
+            duration: 1,
+            ease: 'power3.inOut',
+            onUpdate: () => {
+                camera.position.set(camera.position.x, camera.position.y, camera.position.z)
+            }
+        })
+
+        // camera.position.set(currentIntersect.object.position.x + 10, currentIntersect.object.position.y, currentIntersect.object.position.z + 10)
+    }
+})
+
+
+
+
+
 
 /**
  * Camera
@@ -134,7 +185,6 @@ renderer.setClearColor(0xffffff, 1)
 
 
 const controls = new FirstPersonControls(camera, renderer.domElement)
-// controls.movementSpeed = 150;
 controls.maxDistance = 20
 controls.lookSpeed = 0.1
 
@@ -143,11 +193,38 @@ controls.lookSpeed = 0.1
  */
 const clock = new THREE.Clock()
 let previousTime = 0
+let currentIntersect
 
 const tick = () => {
+    stats.begin()
+
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
+
     previousTime = elapsedTime
+
+    // Raycaster
+    raycaster.setFromCamera(mouse, camera)
+
+    const object = torus
+    const intersect = raycaster.intersectObject(object)
+
+    if (intersect.length) {
+        if (currentIntersect) {
+            console.log(currentIntersect);
+            currentIntersect.object.material.color.set(0xff00ff)
+            document.body.style.cursor = 'pointer'
+        }
+
+        currentIntersect = intersect[0]
+    } else {
+        if (!currentIntersect) {
+            object.material.color.set(0x000000)
+            document.body.style.cursor = 'default'
+        }
+
+        currentIntersect = null
+    }
 
     // Update controls
     controls.update(deltaTime)
@@ -157,6 +234,8 @@ const tick = () => {
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
+    stats.end()
 }
 
 tick()
