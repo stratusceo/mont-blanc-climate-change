a1ffca878fe69d96c6d7e073a91d2148a79b6656
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2022-08-21 14:29:18 +0200

    v1.0

--- src/script.js
+++ src/script.js
@@ -2,7 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-// import * as dat from 'lil-gui'
+import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import gsap from 'gsap'
 
 import Stats from 'stats.js'
@@ -11,28 +12,15 @@ const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom)
 
+gsap.registerPlugin(ScrollToPlugin)
+gsap.registerPlugin(ScrollTrigger)
 
-let actived = false
+let pointActived = false
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.clear();
-
-gsap.set('.wrapper', { x: '-50%' })
-
+/**
+ * HTML Animations
+ */
+// Loader
 const boxWidth = 2000,
     totalWidth = boxWidth * 2,  //  * n of boxes
     no01 = document.querySelectorAll("#no01 .box"),
@@ -42,7 +30,7 @@ const boxWidth = 2000,
 
 const mod = gsap.utils.wrap(0, totalWidth);
 
-function marquee(which, time, direction) {
+const marquee = (which, time, direction) => {
     gsap.set(which, {
         x: function (i) {
             return i * boxWidth;
@@ -63,33 +51,78 @@ function marquee(which, time, direction) {
     return action
 }
 
-const master = gsap.timeline({ play: true })
+gsap.timeline({ play: true })
     .add(marquee(no01, 15, dirFromLeft))
     .add(marquee(no02, 15, dirFromRight), 0)
 
+// Navbar
+const navbarButton = document.getElementById('navbar-button'),
+    menuToggle = new gsap.timeline({ paused: true, reversed: true });
+menuToggle
+    .add('rotate')
+    .to('#navbar-button .mid', .2, { scale: 0.1, opacity: 0, transformOrigin: '50% 50%' }, 'burg')
+    .to('#navbar-button .top', .2, { y: '7' }, 'rotate')
+    .to('#navbar-button .bot', .2, { y: '-7' }, 'rotate')
+    .to('#navbar-button .top', .2, { rotationZ: 45, transformOrigin: '50% 50%' }, 'rotate')
+    .to('#navbar-button .bot', .2, { rotationZ: -45, transformOrigin: '50% 50%' }, 'rotate')
 
+navbarButton.onclick = async () => {
+    
+    if (menuToggle.reversed()) { // show menu
+        menuToggle.restart()
+        
+        const menuElement = document.createElement('div')
+        const menu = await fetch('pages/navbar/menu.html').then(res => res.clone().text())
 
+        menuElement.innerHTML = menu
+        menuElement.id = 'menu'
 
+        document.body.appendChild(menuElement)
 
+        gsap.fromTo('nav', {
+            height: 0
+        }, {
+            height: '100vh',
+            duration: 0.5,
+            ease: 'linear',
+            onComplete: () => {
+                gsap.to('nav li button', {
+                    y: 0,
+                    duration: 1.5,
+                    ease: 'power3.out',
+                    stagger: 0.1
+                });
+            }
+        })
+    } else { // hide menu
+        menuToggle.reverse()
 
+        gsap.to('nav li button', {
+            y: '100%',
+            duration: 0.5,
+            ease: 'power3.out',
+            stagger: 0.1,
+            onComplete: () => {
+                gsap.to('nav', {
+                    height: 0,
+                    duration: 0.5,
+                    ease: 'linear',
+                    onComplete: () => {
+                        document.getElementById('menu').remove()
+                    }
+                })
+            }
+        })
+    }
+}
 
-
-
-
-
-
-
-
-
-/**
- * HTML Animations
- */
+// Points
 const handleShowAnimation = (title, element) => {
     if (!document.getElementById('text-point')) {
         const container = document.createElement('div')
 
         element.onmouseover = () => {
-            if (!actived) {
+            if (!pointActived && activeState === 0) {
                 const text = document.createElement('p')
 
                 text.innerHTML = title
@@ -100,7 +133,7 @@ const handleShowAnimation = (title, element) => {
 
                 snow.classList.add('snow')
 
-                container.append(text, snow)
+                container.append(text)
 
                 document.body.appendChild(container)
 
@@ -123,7 +156,7 @@ const handleShowAnimation = (title, element) => {
         }
 
         element.onmouseleave = () => {
-            if (!actived) gsap.to(container, {
+            if (!pointActived && activeState === 0) gsap.to(container, {
                 opacity: 0,
                 curosr: 'grab',
                 duration: 0.5,
@@ -143,9 +176,7 @@ document.querySelectorAll('.point').forEach(element => {
 /**
  * Base
  */
-// Debug
-// const gui = new dat.GUI()
-
+// Points
 const points = [
     {
         position: new THREE.Vector3(-5.2, 4.85, -5.3),
@@ -159,7 +190,6 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
 scene.fog = new THREE.Fog(0xffffff, 0.1, 75)
 
 /**
@@ -172,17 +202,6 @@ const light = new THREE.HemisphereLight(0x77BAF5, 0x77BAF5, 1);
 light.position.set(0, 0.4, 0)
 light.intensity = 1
 scene.add(light);
-
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-// directionalLight.castShadow = true
-// directionalLight.shadow.mapSize.set(1024, 1024)
-// directionalLight.shadow.camera.far = 15
-// directionalLight.shadow.camera.left = - 7
-// directionalLight.shadow.camera.top = 7
-// directionalLight.shadow.camera.right = 7
-// directionalLight.shadow.camera.bottom = - 7
-// directionalLight.position.set(5, 5, 0)
-// scene.add(directionalLight)
 
 /**
  * Sizes
@@ -212,18 +231,7 @@ window.addEventListener('resize', () => {
 const gltfLoader = new GLTFLoader()
 let model
 
-// const textureLoader = new THREE.TextureLoader()
-// const texture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_baseColor.jpeg')
-// const metallicTexture = textureLoader.load('models/MontBlanc/textures/MontBlanc_VandeHei_1M_u0_v0.001_metallicRoughness.png')
-// texture.minFilter = THREE.LinearFilter
-
 gltfLoader.load('models/mont_blanc_massif_photographed_from_iss/scene.gltf', (gltf) => {
-    // gltf.scene.traverse(child => {
-    //     if (child.isMesh) {
-    //         child.material.wireframe = true
-    //     }
-    // })
-
     const childrens = [...gltf.scene.children]
 
     for (const children of childrens) {
@@ -244,37 +252,26 @@ gltfLoader.load('models/mont_blanc_massif_photographed_from_iss/scene.gltf', (gl
             opacity: 0,
             duration: 1,
             onComplete: () => {
-                gsap.to('#load h1', {
-                    opacity: 0,
+                gsap.fromTo(model.position, {
+                    x: model.position.x,
+                    y: 25,
+                    z: model.position.z
+                }, {
+                    x: 0,
+                    y: 0,
+                    z: 0,
                     duration: 1,
+                    ease: 'back.inOut(2.4)',
+                    onUpdate: () => {
+                        model.position.y = model.position.y
+                    }
                 })
 
-                gsap.to('#load h2', {
+                gsap.to('#load', {
                     opacity: 0,
-                    duration: 1,
+                    duration: 2,
                     onComplete: () => {
-                        gsap.fromTo(model.position, {
-                            x: model.position.x,
-                            y: 25,
-                            z: model.position.z
-                        }, {
-                            x: 0,
-                            y: 0,
-                            z: 0,
-                            duration: 1,
-                            ease: 'back.inOut(2.4)',
-                            onUpdate: () => {
-                                model.position.y = model.position.y
-                            }
-                        })
-
-                        gsap.to('#load', {
-                            opacity: 0,
-                            duration: 2,
-                            onComplete: () => {
-                                document.querySelector('#load').remove()
-                            }
-                        })
+                        document.querySelector('#load').remove()
                     }
                 })
             }
@@ -294,15 +291,7 @@ gltfLoader.load('models/mont_blanc_massif_photographed_from_iss/scene.gltf', (gl
 /**
  * Mouse
  */
-// const mouse = new THREE.Vector2()
-// const raycaster = new THREE.Raycaster()
-
 let activeState = 0
-
-// window.addEventListener('mousemove', (event) => {
-//     mouse.x = (event.clientX / sizes.width) * 2 - 1
-//     mouse.y = -(event.clientY / sizes.height) * 2 + 1
-// })
 
 /**
  * Camera
@@ -342,7 +331,7 @@ const handleShowMenu = element => {
         ease: 'back.in(1.4)',
         onComplete: () => {
             element.remove()
-            actived = false
+            pointActived = false
         }
     })
 
@@ -390,65 +379,114 @@ const tick = async () => {
 
     controls.update()
 
-    // Raycaster
-    // raycaster.setFromCamera(mouse, camera)
-
-    // const object = torus
-    // const intersect = raycaster.intersectObject(object)
-
     switch (activeState) {
         case 1:
+            document.body.style.overflow = 'scroll'
             controls.target.copy(points[0].position)
 
-            if (!document.getElementById('popup')) {
+            if (!document.querySelector('article')) {
                 // add text
-                const popupContainer = document.createElement('div')
                 const articleContainer = document.createElement('article')
 
-                const popup = await fetch('articles/MontBlanc/popup.html').then(res => res.clone().text())
-                const article = await fetch('articles/MontBlanc/article.html').then(res => res.clone().text())
-
-                popupContainer.id = 'popup'
-                popupContainer.innerHTML = popup
-
-                popupContainer.addEventListener('scroll', event => {
-                    const image = document.querySelector('#thumbnail img')
-                    const scrollY = event.target.scrollTop
-
-                    image.style.transform = `translateY(${scrollY / 2}px)`
-                })
+                const article = await fetch('pages/articles/montblanc.html').then(res => res.clone().text())
 
                 articleContainer.innerHTML = article
 
-                document.body.append(popupContainer, articleContainer)
+                document.body.appendChild(articleContainer)
 
-                // add close button
-                const button = document.createElement('button')
+                // image parallax effect
+                let getRatio = element => window.innerHeight / (window.innerHeight + element.offsetHeight);
 
-                button.innerHTML = '<img src="assets/icons/cross.png" />'
-                button.id = 'close-button'
-                button.onclick = e => {
+                gsap.utils.toArray("article div#thumbnail img").forEach((element, i) => {
+                    gsap.to(element, {
+                        y: () => `${(-window.innerHeight * (1 - getRatio(element))) / 2}px`,
+                        ease: "none",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top bottom",
+                            end: "bottom top",
+                            scrub: true,
+                            markers: false,
+                            invalidateOnRefresh: true
+                        }
+                    });
+                });
+
+                // add view more button
+                const moreButton = document.createElement('button')
+
+                moreButton.id = 'more-button'
+                moreButton.innerHTML = `
+                    <p>View more</p>
+                    <img src="assets/icons/arrow.webp" />
+                `
+                moreButton.onmouseover = () => {
+                    gsap.to('#more-button p', {
+                        rotateZ: -15,
+                        duration: 0.5,
+                        ease: 'back.out(1.4)'
+                    })
+                }
+                moreButton.onmouseleave = () => {
+                    gsap.to('#more-button p', {
+                        rotateZ: -10,
+                        duration: 0.5,
+                        ease: 'back.out(1.4)'
+                    })
+                }
+                moreButton.onclick = e => {
                     e.preventDefault()
                     e.stopPropagation()
 
-                    handleShowMenu(popupContainer)
+                    // scroll to article
+                    gsap.to(window, {
+                        scrollTo: 'article',
+                        duration: 1.5,
+                        ease: 'power4.inOut'
+                    });
                 }
 
-                popupContainer.querySelector('#thumbnail').appendChild(button)
+                // add close button
+                const closeButton = document.createElement('button')
 
-                // add view more button
-                // ...
+                closeButton.innerHTML = `
+                    <p>Close</p>
+                    <img src="assets/icons/cross.png" />
+                `
+                closeButton.id = 'close-button'
+                closeButton.onclick = e => {
+                    e.preventDefault()
+                    e.stopPropagation()
 
-                gsap.fromTo(popupContainer, {
-                    y: -50,
-                    opacity: 0,
+                    handleShowMenu(articleContainer)
+                    document.querySelector('article').remove()
+
+                    gsap.to([closeButton, moreButton], {
+                        opacity: 0,
+                        duration: 0.5,
+                        onComplete: () => {
+                            closeButton.remove()
+                            moreButton.remove()
+                        }
+                    })
+                }
+
+                document.body.appendChild(closeButton)
+
+                gsap.fromTo(moreButton, {
+                    opacity: 0
                 }, {
-                    y: 0,
                     opacity: 1,
                     duration: 0.5,
-                    ease: 'power3.inOut'
+                    ease: 'back.in(1.4)'
                 })
+
+                document.body.appendChild(moreButton)
             }
+            break
+
+        case 0:
+            document.body.style.overflow = 'hidden'
             break
     }
 
@@ -466,7 +504,7 @@ const tick = async () => {
         point.element.onclick = () => {
             console.log(point.index, 'point.index');
 
-            actived = true
+            pointActived = true
 
             const textPoint = document.getElementById('text-point')
 
@@ -476,7 +514,7 @@ const tick = async () => {
                 duration: 0.3,
                 ease: 'power2.inOut',
                 onComplete: () => {
-                    textPoint.remove()
+                    if (textPoint) textPoint.remove()
 
                     // transition to description
                     switch (point.index) {
